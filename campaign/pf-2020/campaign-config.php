ec77614c205a4e2597a48b8ac3ab02298a6d0101
Jiří Zapletal <zapletal@wakers.cz>
2019-12-22 18:12:26 +0100

    Removed PF2020 campaign

--- campaign/pf-2020/campaign-config.php
+++
@@ -1,6 +0,0 @@
-<?php return [
-    'utm_params' => 'utm_source=newsletter&utm_medium=email&utm_campaign=pf-2020', // GA Tracking
-    'campaign_url' => 'http://newsletter.wakers.cz/campaign/pf-2020/', // Public path
-    'subject' => 'Vánoční nadílka od Wakers 🎁',
-    'preview_text' => 'Vážení klienti, Vy kteří jste s námi od úplného začátku, myslím, že si zasloužíte ocenit. Než Vám ale dáme  nadílku, musíme se trošku pochlubit.', // 150 chars
-];
--- campaign/pf-2020/email-list.php
+++
@@ -1,6 +0,0 @@
-<?php return [
-    // Outlook, Apple Mail, Gmail, Seznam, Centrum, Spark
-    'wakers.newsletter@gmail.com',
-    'wakers.newsletter@centrum.cz',
-    'wakers.newsletter@seznam.cz',
-];
